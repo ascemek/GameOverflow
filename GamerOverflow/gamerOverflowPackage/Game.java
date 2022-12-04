462599c68e2807a9fc72a2c1b2a417abb414f2ef
Zachary East <91677985+ZiongaB@users.noreply.github.com>
2022-12-04 18:32:47 -0500

    Updated Game to allow unique win/loss states
    Added new booleans to determine if win or loss state is active. 
    Added a 2 methods to allow these variables to be updated.
    Added an abstract method checkVictory() that happens after every computer turn. This is meant to decide whether or not the game should end and can be custom to each game type.

--- GamerOverflow/gamerOverflowPackage/Game.java
+++ GamerOverflow/gamerOverflowPackage/Game.java
@@ -9,70 +9,93 @@ import java.util.Scanner;
  */
 
 public abstract class Game {
-    
-    
+
+    boolean victory = false;
+    boolean lose = false;
 
     /**
-     * The ComputerTurn method is supposed to decide what the enemy will do in response
+     * Checks what the current game status is (this is decided based on user requirements)
+     */
+    abstract void checkVictory(Player p1, NPC e);
+
+    /**
+     * Sets the current state to victory.
+     */
+    public void setVictory()
+    {
+        victory = true;
+    }
+
+    /**
+     * Sets the current state to lost. 
+     */
+    public void setLose()
+    {
+        lose = true;
+    }
+
+
+    /**
+     * The computerTurn method is supposed to decide what the enemy will do in response
      * to the player's input. It will then display the results of the player's and enemy's actions.
      * 
      * @param npc This is an NPC Enemy Character to fight against
      */
-    abstract void ComputerTurn(NPC npc);
+    abstract void computerTurn(NPC npc);
 
     /**
-     * The PlayerTurn Method is supposed to simply prompt the player with the actions that they 
+     * The playerTurn Method is supposed to simply prompt the player with the actions that they 
      * can choose have their character do.
      * 
      * @param p Player character used in the game that performs the actions the player chooses
      * @param sca The scanner that parses the user's input 
      */
-    abstract void PlayerTurn(Player p, Scanner sca);
+    abstract void playerTurn(Player p, Scanner sca);
 
     /**
      * Updates the in game score based on whatever scoring method your game uses
      * 
      * @return An int representing the current score 
      */
-    abstract int UpdateScore();
+    abstract int updateScore();
 
     /**
      * Displays a message informing the player they win. 
      * Then updates the database of scores with the user's score.
      */
-    abstract void VictoryEnd();
+    abstract void victoryEnd();
 
     /**
      * Displays a message informing the player they lose. 
      * Then updates the database of scores with the user's score.
      */
-    abstract void LoseEnd();
+    abstract void loseEnd();
 
 
     public void gameStart(Scanner scan, Player player1, NPC computerNpc){
 
         boolean gameOn = true;
-
-        int player1Health = player1.getHealth();
-        int npcHealth = computerNpc.getHealth();
+        //int player1Health = player1.getHealth();
+        //int npcHealth = computerNpc.getHealth();
 
 
         while(gameOn)
         {
-            PlayerTurn(player1, scan);
-            ComputerTurn(computerNpc);
-            UpdateScore();
+            playerTurn(player1, scan);
+            computerTurn(computerNpc);
+            updateScore();
+            checkVictory(player1, computerNpc);
 
-            if((player1Health <= 0))
+            if((victory =true))
             {
                 gameOn = false;
-                LoseEnd();
+                loseEnd();
             }
 
-            if((npcHealth <= 0))
+            if((lose = true))
             {
                 gameOn = false;
-                VictoryEnd();
+                victoryEnd();
             }
             
         }
